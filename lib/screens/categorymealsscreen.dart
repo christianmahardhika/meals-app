ddc0d11087974970b73021f066348054af735e06
Christian <christian.mahardhika@gmail.com>
2021-03-11 10:45:48 +0700

    feat: more widgets

---
+++ lib/screens/categorymealsscreen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meals.dart';
+import 'package:meals_app/widgets/mealitem.dart';
+import '../dummydata.dart';
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const routeName = '/category-meals';
+
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen({this.availableMeals});
+
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  var _loadedInitData = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+      final routesArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+      categoryTitle = routesArgs['title'];
+      final categoryId = routesArgs['id'];
+      displayedMeals = widget.availableMeals
+          .where(
+            (meal) => meal.categories.contains(categoryId),
+          )
+          .toList();
+      _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            id: displayedMeals[index].id,
+            title: displayedMeals[index].title,
+            imageUrl: displayedMeals[index].imageUrl,
+            affordability: displayedMeals[index].affordability,
+            complexity: displayedMeals[index].complexity,
+            duration: displayedMeals[index].duration,
+          );
+        },
+        itemCount: displayedMeals.length,
+      ),
+    );
+  }
+}
